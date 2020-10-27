6f7ad6750d769a305035a969f1abdd465925a4c1
xandervdh <xander-v.d.h@hotmail.com>
2020-10-27 11:53:48 +0100

    started making my code mvc

---
+++ controller/RegisterController.php
@@ -0,0 +1,103 @@
+<?php
+declare(strict_types=1);
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+require 'model/Auth.php';
+require 'model/Connection.php';
+require 'model/Student.php';
+class RegisterController {
+    public function render()
+    {
+        session_start();
+
+        $authorization = new Auth();
+        $error = 'style="border-color: red"';
+        $firstName = $lastName = $email = $password = $passwordConfirm = "";
+        $firstNameError = $lastNameError = $emailError = $passwordError = $passwordConfirmError = "";
+        $firstNameErrorMessage = $lastNameErrorMessage = $emailErrorMessage = $passwordErrorMessage = $passwordConfirmErrorMessage = "";
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if (empty($_POST['firstName'])) {
+                $firstNameErrorMessage = 'First name is required';
+                $firstNameError = $error;
+            } else {
+                $firstName = $this->check_input($_POST['firstName']);
+                $firstNameErrorMessage = $authorization->nameValidation($firstName);
+                if (!empty($firstNameErrorMessage)) {
+                    $firstNameError = $error;
+                } else {
+                    $_SESSION['firstName'] = $firstName;
+                }
+            }
+
+            if (empty($_POST['lastName'])) {
+                $lastNameErrorMessage = 'Last name is required';
+                $lastNameError = $error;
+            } else {
+                $lastName = $this->check_input($_POST['lastName']);
+                $lastNameErrorMessage = $authorization->nameValidation($lastName);
+                if (!empty($lastNameErrorMessage)) {
+                    $lastNameError = $error;
+                } else {
+                    $_SESSION['lastName'] = $lastName;
+                }
+            }
+
+            if (empty($_POST['email'])) {
+                $emailErrorMessage = 'Email is required';
+                $emailError = $error;
+            } else {
+                $email = $_POST['email'];
+                $emailErrorMessage = $authorization->emailValidation($email);
+                if (!empty($emailErrorMessage)) { //check if input has a valid email
+                    $emailError = $error; //give error style
+                } else {
+                    $_SESSION['email'] = $email;
+                }
+            }
+
+            if (empty($_POST['password'])) {
+                $passwordErrorMessage = 'Password is required';
+                $passwordError = $error;
+            }
+
+            if (empty($_POST['passwordConfirm'])) {
+                $passwordConfirmErrorMessage = 'Password confirmation is required';
+                $passwordConfirmError = $error;
+            }
+
+            if (empty($passwordConfirmErrorMessage) && empty($passwordErrorMessage)) {
+                $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+                $passwordConfirm = $_POST['passwordConfirm'];
+                $passwordErrorMessage = $authorization->passwordValidation($password, $passwordConfirm);
+                if (!empty($passwordErrorMessage)) {
+                    $passwordConfirmError = $error;
+                    $passwordError = $error;
+                    $passwordConfirmErrorMessage = $passwordErrorMessage;
+                } else {
+                    $_SESSION['password'] = $password;
+                    $_SESSION['passwordConfirm'] = $passwordConfirm;
+                }
+            }
+
+            if (empty($firstNameErrorMessage) && empty($lastNameErrorMessage) && empty($emailErrorMessage) && empty($passwordErrorMessage)) {
+                $student = new Student($firstName, $lastName, $email, $password);
+                $connection = new Connection();
+                $connection->insertData($student);
+            }
+        }
+
+        require 'view/register.php';
+    }
+
+    public function check_input($data)
+    {
+        $data = trim($data); //remove whitespace from beginning and end of input
+        $data = stripslashes($data); //remove slashes
+        $data = htmlspecialchars($data); //changes html elements to characters
+        return $data;
+    }
+}
---
+++ view/register.php
@@ -0,0 +1,34 @@
+<?php require 'includes/header.php' ?>
+
+<div id="form">
+    <span class="required">* = required</span>
+    <form method="post" >
+        <label for="firstName">First name</label><br>
+        <input type="text" name="firstName" id="firstName"
+               value="<?php echo $firstName ?>" <?php echo $firstNameError; ?>><br>
+        <span class="required">* <?php echo $firstNameErrorMessage; ?></span><br>
+
+        <label for="lastName">Last name</label><br>
+        <input type="text" name="lastName" id="lastName"
+               value="<?php echo $lastName ?>" <?php echo $lastNameError; ?>><br>
+        <span class="required">* <?php echo $lastNameErrorMessage; ?></span><br>
+
+        <label for="email">Email</label><br>
+        <input type="text" name="email" id="email" value="<?php echo $email ?>" <?php echo $emailError; ?>><br>
+        <span class="required">* <?php echo $emailErrorMessage; ?></span><br>
+
+        <label for="password">Password</label><br>
+        <input type="password" name="password" id="password"
+            <?php echo $passwordError; ?>><br>
+        <span class="required">* <?php echo $passwordErrorMessage; ?></span><br>
+
+        <label for="passwordConfirm">Confirm password</label><br>
+        <input type="password" name="passwordConfirm" id="passwordConfirm"
+            <?php echo $passwordConfirmError; ?>><br>
+        <span class="required">* <?php echo $passwordConfirmErrorMessage; ?></span><br>
+
+        <input type="submit" value="Submit">
+    </form>
+</div>
+
+<?php require 'includes/footer.php' ?>
