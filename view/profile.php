4e1a6fd7681cc4172d3a79a745dbadbc6b279985
xandervdh <xander-v.d.h@hotmail.com>
2020-10-29 12:15:33 +0100

    gave the profile some styling

--- view/profile.php
+++ view/profile.php
@@ -1,15 +1,24 @@
 <?php require 'includes/header.php'; ?>
 <div>
-    <a href="http://mysqlintroduction.local/?page=overview">Overview</a><br>
-<?php
-echo 'Name: ' . $student['first_name'] . ' ' . $student['last_name'] . '<br>Email: ' . $student['email'] . '<br>Created at: ' . $student['created_at'];
 
-?>
-    <br><img width="300px" src="<?php echo $student['image'] ?>" alt="kitty">
-    <form style="<?php echo $show ?>" action="" method="post">
-        <input type="hidden" name="user" value="<?php echo $id['id'] ?>">
-        <input type="submit" name="action" value="Edit">
-        <input type="submit" name="action" value="Delete">
-    </form>
+    <div class="container" style="margin-top: 20px; margin-bottom: 20px;">
+        <a href="http://mysqlintroduction.local/?page=overview">Overview</a>
+        <div class="row panel">
+            <div class="col-md-8  col-xs-12">
+                <img alt="profile picture" src="<?php echo $student['image'] ?>" class="img-thumbnail picture hidden-xs" />
+                <div class="header">
+                    <h1><?php echo $student['first_name'] . ' ' . $student['last_name']; ?></h1>
+                    <h4>Email: <?php echo $student['email']; ?></h4>
+                    <span>Created at: <?php echo $student['created_at']; ?>"</span>
+                </div>
+            </div>
+        </div>
+        <form style="<?php echo $show ?>" id="profileButtons" action="" method="post">
+            <input type="hidden" name="user" value="<?php echo $id['id'] ?>">
+            <input type="submit" name="action" value="Edit">
+            <input type="submit" name="action" value="Delete">
+        </form>
+    </div>
+
 </div>
 <?php require 'includes/footer.php'; ?>
