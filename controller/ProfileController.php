b8fcd4475e41ca2d8c26cc4472c7c4450a7fb859
xandervdh <xander-v.d.h@hotmail.com>
2020-10-29 09:44:15 +0100

    added cat picture on profile

--- controller/ProfileController.php
+++ controller/ProfileController.php
@@ -42,6 +42,8 @@ class ProfileController
                 }
             }
         }
+        $cat = file_get_contents("https://api.thecatapi.com/v1/images/search");
+        $catPicture = json_decode($cat, true);
         require $view;
     }
 }
